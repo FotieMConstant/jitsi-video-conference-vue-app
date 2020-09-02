9d98f5a75e6224f07351ba56c27f2da2c488e7c5
FotieMConstant <fotiemoghommahieconstant@gmail.com>
2020-09-02 11:02:45 +0100

    Final implementation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <h2>Crtv-fm app</h2>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="http://www.crtv.cm/crtv-yaounde-fm94/" target="_blank" text>
+        <span class="mr-2">Official website</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <VideoConference />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VideoConference from "./components/VideoConference";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    VideoConference,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
